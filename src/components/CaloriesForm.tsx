69b2f57bf287b3b0751f1dee8fc285ffbf435152
William Gustavo Santisteban <rootsantisteban@gmail.com>
2024-07-12 12:00:06 -0300

    Change uuid + add @types/uuid
    Change uuid + add @types/uuid

--- src/components/CaloriesForm.tsx
+++ src/components/CaloriesForm.tsx
@@ -1,5 +1,5 @@
 import { ChangeEvent, Dispatch, useEffect, useState } from "react";
-import {v4 as uuidv4} from 'uuid';
+import { v4 as uuidv4 } from "uuid";
 import { categories } from "../data/categories";
 import { Activity } from "../types";
 import { ActivityActions, ActivityState } from "../reducers/activityReducer";
